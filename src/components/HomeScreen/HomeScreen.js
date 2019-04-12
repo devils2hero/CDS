33be32fca42cec5226ffdcda13e60946ca493c30
devils2hero <hungs2you@gmail.com>
2019-04-12 08:52:47 +0700

    Create HomeScreen.js

---
+++ src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { Icon, SearchBar, TabBar } from "@ant-design/react-native";
+export default class HomeScreen extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <View>
+        <Text>Ahihi</Text>
+      </View>
+    );
+  }
+}
