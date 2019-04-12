7f6afb6f38c8395f3e36784c072642c7e3a6e261
devils2hero <hungs2you@gmail.com>
2019-04-12 08:53:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import { AppRegistry } from "react-native";
+import CDS from "./src/components/HomeScreen/index";
+import Login from "./src/components/LoginScreen/Login";
+import Register from "./src/components/RegisterScreen/Register";
+import SplashScreen from "./src/components/SplashScreen/SplashScreen";
+import { name as appName } from "./app.json";
+AppRegistry.registerComponent(appName, () => CDS);
