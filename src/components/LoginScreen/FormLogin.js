bd2a07068a97b5435c2e95abad05d937b5ce7388
devils2hero <hungs2you@gmail.com>
2019-04-12 08:52:32 +0700

    Create FormLogin.js

---
+++ src/components/LoginScreen/FormLogin.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Text, View, TextInput, Image, Alert } from "react-native";
+
+export default class FormLogin extends Component {
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            backgroundColor: "#FFFFFF",
+            borderRadius: 30,
+            width: 280,
+            height: 40,
+            marginBottom: 12,
+            flexDirection: "row",
+            alignItems: "center"
+          }}
+        >
+          <TextInput
+            style={{
+              height: 40,
+              marginLeft: 16,
+              flex: 1,
+              color: "#727C8E",
+              fontSize: 15
+            }}
+            placeholderTextColor="#727C8E"
+            placeholder="Số điện thoại"
+            keyboardType="phone-pad"
+            underlineColorAndroid="transparent"
+            onChangeText={email => this.setState({ email })}
+          />
+        </View>
+        <View
+          style={{
+            backgroundColor: "#FFFFFF",
+            borderRadius: 30,
+            width: 280,
+            height: 40,
+            marginBottom: 10,
+            flexDirection: "row",
+            alignItems: "center"
+          }}
+        >
+          <TextInput
+            style={{
+              height: 45,
+              marginLeft: 16,
+              flex: 1,
+              color: "#727C8E",
+              fontSize: 15
+            }}
+            placeholderTextColor="#727C8E"
+            placeholder="Mật khẩu"
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+            onChangeText={password => this.setState({ password })}
+          />
+        </View>
+      </View>
+    );
+  }
+}
