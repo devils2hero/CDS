d2bcfffab0bae8ea6eba7d83780cc1e8b6b8dab1
devils2hero <hungs2you@gmail.com>
2019-04-12 08:48:55 +0700

    Create SplashScreen.js

---
+++ src/components/SplashScreen/SplashScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { View, ImageBackground } from "react-native";
+import Logo from "../LoginScreen/Logo";
+import Slogan from "../SplashScreen/Slogan";
+import ButtonStart from "./ButtonStart";
+import TextBottom from "../LoginScreen/TextBottom";
+export default class SplashScreen extends Component {
+  render() {
+    return (
+      <View>
+        <ImageBackground
+          style={{ width: "100%", height: "100%" }}
+          source={require("../../assets/images/backgroundlogin.png")}
+        >
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <Logo />
+            <Slogan />
+            <ButtonStart />
+          </View>
+          <TextBottom />
+        </ImageBackground>
+      </View>
+    );
+  }
+}
