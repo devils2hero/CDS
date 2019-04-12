4a3b6d9dfb4def201532d1552436dc68a9706203
devils2hero <hungs2you@gmail.com>
2019-04-12 08:52:35 +0700

    Create ButtonSubmit.js

---
+++ src/components/LoginScreen/ButtonSubmit.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Text
+} from "react-native";
+export default class ButtonSubmit extends Component {
+  constructor() {
+    super();
+  }
+  onClickListener = viewId => {
+    Alert.alert("Alert", "Button pressed" + viewId);
+  };
+  render() {
+    return (
+      <View>
+        <TouchableHighlight
+          style={{
+            height: 40,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 20,
+            width: 200,
+            borderRadius: 30,
+            backgroundColor: "#FF9800"
+          }}
+        >
+          <Text style={{ color: "#FFFFFF", fontWeight: "bold", fontSize: 15 }}>
+            ĐĂNG NHẬP
+          </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
