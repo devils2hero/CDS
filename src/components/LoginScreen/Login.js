5d785d34912184e081b8c42ca7d68347d066cf91
devils2hero <hungs2you@gmail.com>
2019-04-12 08:52:24 +0700

    Create Login.js

---
+++ src/components/LoginScreen/Login.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Button,
+  Image,
+  Alert,
+  ImageBackground
+} from "react-native";
+import ButtonSubmit from "./ButtonSubmit";
+import FormLogin from "./FormLogin";
+import Logo from "./Logo";
+import TextRegister from "./TextRegister";
+import FotgotPasswordText from "./FotgotPasswordText";
+import TextBottom from "./TextBottom";
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <ImageBackground
+          style={{ width: "100%", height: "100%" }}
+          source={require("../../assets/images/backgroundlogin.png")}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Logo />
+            <FormLogin />
+            <View style={{ flexDirection: "row" }}>
+              <FotgotPasswordText />
+            </View>
+            <ButtonSubmit />
+            <TextRegister />
+          </View>
+          <TextBottom />
+        </ImageBackground>
+      </View>
+    );
+  }
+}
