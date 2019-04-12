b0431d30b8c586d0128c435487266854e37962be
devils2hero <hungs2you@gmail.com>
2019-04-12 08:52:38 +0700

    Create QuestionScreen.js

---
+++ src/components/HomeScreen/QuestionScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { Icon, SearchBar, TabBar } from "@ant-design/react-native";
+export default class QuestionScreen extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <View>
+        <Text>Ahihi 1</Text>
+      </View>
+    );
+  }
+}
