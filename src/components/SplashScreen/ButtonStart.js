3301e574134e207aa3c03add974f2684646da308
devils2hero <hungs2you@gmail.com>
2019-04-12 10:29:33 +0700

    Update ButtonStart.js

--- src/components/SplashScreen/ButtonStart.js
+++ src/components/SplashScreen/ButtonStart.js
@@ -17,31 +17,29 @@ export default class ButtonStart extends Component {
   render() {
     return (
       <View>
-        <TouchableHighlight
-          style={{
-            marginTop: 100,
-            height: 40,
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: 20,
-            width: 200,
-            borderRadius: 30,
-            backgroundColor: "#FF9800"
-          }}
-        >
-          <Text
-            style={{
-              color: "#FFFFFF",
-              fontWeight: "bold",
-              fontSize: 15,
-              fontWeight: "normal"
-            }}
-          >
-            BẮT ĐẦU
-          </Text>
+        <TouchableHighlight style={styles.buttonContainerSplashScreen}>
+          <Text style={styles.textButtonSplashScreen}>BẮT ĐẦU</Text>
         </TouchableHighlight>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  buttonContainerSplashScreen: {
+    marginTop: 100,
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 200,
+    borderRadius: 30,
+    backgroundColor: "#FF9800"
+  },
+  textButtonSplashScreen: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 15,
+    fontWeight: "normal"
+  }
+});
