ca43ab1b25329eadfe55ef680803957c6818b3e8
devils2hero <hungs2you@gmail.com>
2019-04-16 10:17:09 +0700

    Create dataFlatListCategory.js

---
+++ src/components/HomeScreen/CategoryQuestionScreen/dataFlatListCategory.js
@@ -0,0 +1,45 @@
+var flatListData = [
+  {
+    key: "598a678278fee204ee51cd2c",
+    name: "Kiến thức cơ bản",
+    backgroundColor: "#039BE5",
+    sosao: "136",
+    defaultstar: "200"
+  },
+  {
+    key: "598a684f78fee204ee51cd2f",
+    name: "Làm quen với số đếm",
+    backgroundColor: "#FF9800",
+    sosao: "136",
+    defaultstar: "200"
+  },
+  {
+    key: "598a687678fee204ee51cd30",
+    name: "Làm quen với mặt chữ",
+    backgroundColor: "#F44336",
+    sosao: "136",
+    defaultstar: "200"
+  },
+  {
+    key: "598a680178fee204ee51cd2e",
+    name: "Bất đẳng thức",
+    backgroundColor: "#727C8E",
+    sosao: "136",
+    defaultstar: "200"
+  },
+  {
+    key: "598a688878fee204ee51cd31",
+    name: "Hệ phương trình",
+    backgroundColor: "#1976D2",
+    sosao: "136",
+    defaultstar: "200"
+  },
+  {
+    key: "598a688878fee204ee51cd99",
+    name: "Lập trình C#",
+    backgroundColor: "#FFEB8A",
+    sosao: "136",
+    defaultstar: "200"
+  }
+];
+export default flatListData;
