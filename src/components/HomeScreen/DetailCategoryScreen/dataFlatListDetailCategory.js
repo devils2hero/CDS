16b4f5d520e138d6ed8decf5ba376ae0481ff4d1
devils2hero <hungs2you@gmail.com>
2019-04-16 10:17:08 +0700

    Create dataFlatListDetailCategory.js

---
+++ src/components/HomeScreen/DetailCategoryScreen/dataFlatListDetailCategory.js
@@ -0,0 +1,33 @@
+var flatListData = [
+  {
+    key: "598a678278fee204ee51cd2c",
+    name: "Màu sắc",
+    backgroundColor: "#039BE5"
+  },
+  {
+    key: "598a684f78fee204ee51cd2f",
+    name: "Đồ vật",
+    backgroundColor: "#FF9800"
+  },
+  {
+    key: "598a687678fee204ee51cd30",
+    name: "Động vật",
+    backgroundColor: "#F44336"
+  },
+  {
+    key: "598a680178fee204ee51cd2e",
+    name: "Cây cối",
+    backgroundColor: "#727C8E"
+  },
+  {
+    key: "598a688878fee204ee51cd31",
+    name: "Phương tiện",
+    backgroundColor: "#1976D2"
+  },
+  {
+    key: "598a688878fee204ee51cd99",
+    name: "Kỹ năng 1",
+    backgroundColor: "#FFEB8A"
+  }
+];
+export default flatListData;
